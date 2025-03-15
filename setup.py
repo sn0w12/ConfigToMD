d1fd2f4d7a92512e5a61b3e59321224d50abc433
Lucas  Sköld <lucasskold49@gmail.com>
2025-03-15 01:04:01 +0100

    Fix entry point

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ setup(
     description="Extract config settings from Python files and generate markdown documentation",
     author="Sn0w12",
     packages=find_packages(),
-    py_modules=["scan"],
     entry_points={
         "console_scripts": [
-            "configtomd=scan:main",
+            "configtomd=configtomd.scan:main",
         ],
     },
     python_requires=">=3.6",
